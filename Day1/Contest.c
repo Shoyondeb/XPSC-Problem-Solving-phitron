12e473f11b8f0b150a3f3ae67fb104a5a2396869
Shoyon Deb <106007811+Shoyondeb@users.noreply.github.com>
2024-10-26 14:07:41 +0600

    Add files via upload

---
+++ Day1/Contest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,i,j;
+    printf("Enter one number here:");
+    scanf("%d", &a);
+    for(i=1;i<=a;i++){
+        for(j=1;j<=i;j++){
+            printf("%d %d\n", i,j);
+        }
+    }
+}
