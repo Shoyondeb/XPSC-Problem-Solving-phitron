a18ef2384ad0fc6a1257717ed39af94480c19bb8
Shoyon Deb <106007811+Shoyondeb@users.noreply.github.com>
2024-12-31 00:44:08 +0600

    Update B_Make_N_Zero.cpp

--- Week10 Day2/B_Make_N_Zero.cpp
+++ Week10 Day2/B_Make_N_Zero.cpp
@@ -1,21 +1,35 @@
 #include <bits/stdc++.h>
+#define ll long long int
 using namespace std;
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int res1=n%3;
-        int res2=n%4;
-        if(n<=2){
-            cout<<"NO\n";
-        }else if(res1==0 || res2==0 || res2==3){
-            cout<<"YES\n";
-        }else{
-            cout<<"NO\n";
+    string str;
+    cin>>str;
+    map<char, ll>mp;
+    for(ll i=0; i<str.size(); i++){
+        mp[str[i]]++;
+    }
+    string str1, str2; ll flag=0, count=0;
+    char a;
+    for(pair<char,ll>pr:mp){
+        if(pr.second%2!=0){
+            flag=1;
+            a=pr.first;
+            count++;
+            if(count>=2){
+                cout<<"NO SOLUTION\n";
+                return 0;
+            }
+        }
+        for(ll i=0; i<pr.second/2; i++){
+            str1.push_back(pr.first);
+            str2.push_back(pr.first);
         }
     }
+    reverse(str2.begin(), str2.end());
+    if(flag==1)
+    cout<<str1<<a<<str2<<endl;
+    else
+    cout<<str1<<str2<<endl;
     return 0;
 }
